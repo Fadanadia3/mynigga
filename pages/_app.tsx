64f2770044a60d286e160079549747460ff5d218
Fadanadia3 <fadanadia3@gmail.com>
2025-02-02 16:49:55 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,9 +22,10 @@ import { arbitrum, bsc, gnosis, optimism, polygon } from 'viem/chains';
 import { z } from 'zod';
 import { useIsMounted } from '../hooks';
 
+// Ensure that the environment variable is correctly set in your .env file
 const walletConnectProjectId = z
   .string()
-  .parse(process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID);
+  .parse(process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID);  // Ensure that this variable is correctly set in .env
 
 const { chains, publicClient } = configureChains(
   [mainnet, polygon, optimism, arbitrum, bsc, gnosis],
@@ -33,7 +34,7 @@ const { chains, publicClient } = configureChains(
 
 const { connectors } = getDefaultWallets({
   appName: 'Drain',
-  projectId: walletConnectProjectId,
+  projectId: walletConnectProjectId, // Pass the projectId from environment variable
   chains,
 });
 
