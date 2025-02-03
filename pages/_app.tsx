6deeff653f8db67e0abc84cb978dcf7edc2658b9
Fadanadia3 <fadanadia3@gmail.com>
2025-02-03 12:06:31 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,78 +1,27 @@
-import { CssBaseline, GeistProvider } from '@geist-ui/core';
-import type { AppProps } from 'next/app';
-import NextHead from 'next/head';
-import GithubCorner from 'react-github-corner';
-// @ts-ignore
-import '../styles/globals.css';
-
-// Imports
-import {
-  configureChains,
-  createConfig,
-  mainnet,
-  // createClient,
-  WagmiConfig,
-} from 'wagmi';
-import { publicProvider } from 'wagmi/providers/public';
-
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import '@rainbow-me/rainbowkit/styles.css';
-
-import { arbitrum, bsc, gnosis, optimism, polygon } from 'viem/chains';
+@@ -22,9 +22,10 @@ import { arbitrum, bsc, gnosis, optimism, polygon } from 'viem/chains';
 import { z } from 'zod';
 import { useIsMounted } from '../hooks';
 
+// Ensure that the environment variable is correctly set in your .env file
 const walletConnectProjectId = z
   .string()
   .parse(process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID);
+  .parse(process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID);  // Ensure that this variable is correctly set in .env
 
 const { chains, publicClient } = configureChains(
   [mainnet, polygon, optimism, arbitrum, bsc, gnosis],
-  [publicProvider()],
-);
+@@ -33,7 +34,7 @@ const { chains, publicClient } = configureChains(
 
 const { connectors } = getDefaultWallets({
   appName: 'Drain',
   projectId: walletConnectProjectId,
+  projectId: walletConnectProjectId, // Pass the projectId from environment variable
   chains,
 });
 
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient,
-});
-
-const App = ({ Component, pageProps }: AppProps) => {
-  const isMounted = useIsMounted();
-
-  if (!isMounted) return null;
-  return (
-    <>
-      <GithubCorner
-        href="https://github.com/dawsbot/drain"
-        size="140"
-        bannerColor="#e056fd"
-      />
-
-      <WagmiConfig config={wagmiConfig}>
-        <RainbowKitProvider coolMode chains={chains}>
-          <NextHead>
-            <title>Drain</title>
-            <meta
-              name="description"
-              content="Send all tokens from one wallet to another"
-            />
-            <link rel="icon" href="/favicon.ico" />
-          </NextHead>
-          <GeistProvider>
-            <CssBaseline />
-            <Component {...pageProps} />
-          </GeistProvider>
-        </RainbowKitProvider>
-      </WagmiConfig>
-    </>
+@@ -75,4 +76,4 @@ const App = ({ Component, pageProps }: AppProps) => {
   );
 };
 
+export default App;
 export default App;
