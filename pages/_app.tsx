2c7402a7786bb6f633abdf387bc3e36a2a6a1a6d
Fadanadia3 <fadanadia3@gmail.com>
2025-02-03 12:12:26 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,27 @@
-@@ -22,9 +22,10 @@ import { arbitrum, bsc, gnosis, optimism, polygon } from 'viem/chains';
+import { arbitrum, bsc, gnosis, optimism, polygon } from 'viem/chains';
 import { z } from 'zod';
 import { useIsMounted } from '../hooks';
 
 // Ensure that the environment variable is correctly set in your .env file
 const walletConnectProjectId = z
   .string()
-  .parse(process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID);
-  .parse(process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID);  // Ensure that this variable is correctly set in .env
+  .parse(process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID);  // Use only this variable
 
 const { chains, publicClient } = configureChains(
   [mainnet, polygon, optimism, arbitrum, bsc, gnosis],
-@@ -33,7 +34,7 @@ const { chains, publicClient } = configureChains(
+  // Other configuration...
+);
 
 const { connectors } = getDefaultWallets({
   appName: 'Drain',
-  projectId: walletConnectProjectId,
-  projectId: walletConnectProjectId, // Pass the projectId from environment variable
+  projectId: walletConnectProjectId,  // Only pass it once
   chains,
 });
 
-@@ -75,4 +76,4 @@ const App = ({ Component, pageProps }: AppProps) => {
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <Component {...pageProps} />
   );
 };
 
-export default App;
 export default App;
