d970ba2359683dfdf8ec2cd557babb2c4a5339b4
Fadanadia3 <fadanadia3@gmail.com>
2025-02-07 00:31:26 +0100

    Update contractAddress.js

--- utils/contractAddress.js
+++ utils/contractAddress.js
@@ -1,2 +1,3 @@
-const contractAddress = '0x518c5D62647E60864EcB3826e982c93dFa154af3';
+// utils/contractAddress.js
+const contractAddress = "0x518c5D62647E60864EcB3826e982c93dFa154af3";
 export default contractAddress;
