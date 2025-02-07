425d47f11102be36fc88ff661dc52d6641cdbe52
Fadanadia3 <fadanadia3@gmail.com>
2025-02-07 01:03:48 +0100

    Update contractAddress.js

--- utils/contractAddress.js
+++ utils/contractAddress.js
@@ -1,3 +1,3 @@
 // utils/contractAddress.ts
-const contractAddress: string = "0xb44de2566da86c8cef673899d0b53fa69bd685cf"; // Adresse du contrat
+const contractAddress: `0x${string}` = "0xb44de2566da86c8cef673899d0b53fa69bd685cf"; // Adresse du contrat
 export default contractAddress;
